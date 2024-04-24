5e8c3e66ab33da289f9e502a4092e14e8f3778da
Aditya Gupta <adityaguptacse@gmail.com>
2024-04-24 01:38:15 -0700

    Commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import './App.css';
 function App() {
 
   const [input, setInput] = useState(0)
+
+  const oncleckHandler = (e) => {
+    if(e.target.id ===    "inc") {
+      setInput((prev) => prev+1)
+    }
+    else {
+      setInput((prev) => prev-1)
+    }
+
+  }
   return (
     <div >
       <h1>Counter App</h1>
       <div>
-        <p>Counter: {input}</p>
-        <button onClick={(e) =>{
-            setInput(input+1)
-        }}>Increment</button>
-
-        <button onClick={e =>{
-          setInput(input-1)
-        }}>Decrement</button>
+        <p>Count: {input}</p>
+        <button type="button" id="inc" onClick={oncleckHandler}>
+        Increment
+      </button>
+      <button type="button" id="dec" onClick={oncleckHandler}>
+        Decrement
+      </button>
       </div>
       
     </div>
